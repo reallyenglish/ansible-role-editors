f0e92a94fc50b1fef102d098a2dc13df1807d54c
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-05-24 20:49:30 +0900

    initial import

---
+++ spec/serverspec/default_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'serverspec'
+
+packages = nil
+
+case os[:family]
+when 'freebsd'
+  packages = %w[ vim-lite emacs-nox11 ]
+when 'openbsd'
+  packages = %w[ vim emacs ]
+end
+
+packages.each do |p|
+  describe package(p) do
+    it { should be_installed }
+  end
+end
